5d4f30733e5632aa2ed3d2455dfbfd929c396532
thou <nothou@proton.me>
2025-01-24 20:02:40 -0800

    trying to add functionality of it advertising backup or not depending on current player activity via new "-a" argument on advertise.py

--- backup.sh
+++ backup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-BACKUP_DIRECTORY = $XDG_DATA_HOME/thousmc/backup
+BACKUP_DIRECTORY=$XDG_DATA_HOME/thousmc/backup
+ADVERTISE_DIR=$HOME/docs/software/code/python/advertise/advertise.py  # ill have to change this for each implementation
 
 if ! grep -qE "joined the server|left the server" ~/thousmc/logs/latest.log; then
     echo "No player activity detected. Backup skipped."
 else
     echo "Player activity detected. Running backup..."
-    mkdir -p $BACKUP_DIRECTORY
-
+    if $(python3 $ADVERTISE_DIR -a -s ) == 'True'; then
+        echo 'True!'
+    else
+        echo 'False!'
+    fi
 fi
